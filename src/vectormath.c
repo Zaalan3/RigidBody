b1a7aa05d97798fd231e32b4c74251d5c83c7726
Zachary Love <4642969+Zaalan3@users.noreply.github.com>
2020-05-25 11:40:33 -0500

    Add files via upload

--- src/vectormath.c
+++ src/vectormath.c
@@ -4,15 +4,19 @@
 int normalize(Vec2* v) {
 	int length,invl; 
 	
+	/*
 	int x = abs(v->x);
 	int y = abs(v->y);
-	
+	 
 	if(y>x) { 
 		int temp = x; 
 		x = y; 
 		y = x; 
 	} 
 	length = max(fxMul(254,x)+fxMul(48,y),fxMul(216,x)+fxMul(142,y));
+	*/
+	
+	length = 16*sqrtInt(fxMul(v->x,v->x)+fxMul(v->y,v->y));
 	invl = (256*256)/length; 
 	v->x = fxMul(v->x,invl);
 	v->y = fxMul(v->y,invl);
